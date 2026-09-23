2c4448935ed43cf3a785635b20dd27ab5249c04e
Tomas(tunng 67 dear) <trrodriguez29@setoncatholic.org>
2026-09-23 20:22:58 +0000

    second

--- main.py
+++ main.py
@@ -2,23 +2,23 @@
 #result = eval(expression)
 #print(result)
 
-def add(x,y):return(x + y)
-def sub(x,y):return(x - y)
-def mult(x,y):return(x * y)
+def add(x,y):print(x + y)
+def sub(x,y):print(x - y)
+def mult(x,y):print(x * y)
 def div(x,y):
     if y == 0:print("zero error")
-    else:return x/y
+    else:print(x/y)
 
 
 
-#function adds X and y
+print("welcome user")
 
 while True:
-    d = input("decisionoperation use -,+,/,*:")
-    a,b = int(input("first number:").strip(),input("second number:").strip())
-    if d == "/":div(a,b)
-    if d == "+":add(a,b)
-    if d == "-":sub(a,b)
-    if d == "*":mult(a,b)
+    print("what do you want to do")
+    d,a,b, = input("(a)add,(s)subtract,(m)multiply,(d)divide"),int(input("first number:").strip()),int(input("second number:").strip())
+    if d == "d":div(a,b)
+    if d == "a":add(a,b)
+    if d == "s":sub(a,b)
+    if d == "m":mult(a,b)
     else:
         if input("you done if yes type yes") == "yes":break
