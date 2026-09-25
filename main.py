5bf4b8ec51848eecd54f3c57519e84f09c16d423
Tomas(tunng 67 dear) <trrodriguez29@setoncatholic.org>
2026-09-25 15:27:47 +0000

    pre extracredit version

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 #result = eval(expression)
 #print(result)
 
-def add(x,y):print(x + y)
-def sub(x,y):print(x - y)
-def mult(x,y):print(x * y)
+def add(x,y):print("\n",x + y,"\n")
+def sub(x,y):print("\n",x - y,"\n")
+def mult(x,y):print("\n",x * y,"\n")
 def div(x,y):
     if y == 0:print("zero error")
-    else:print(x/y)
+    else:print("\n",x/y,"\n")
 
 
 
@@ -15,10 +15,12 @@ print("welcome user")
 
 while True:
     print("what do you want to do")
-    d,a,b, = input("(a)add,(s)subtract,(m)multiply,(d)divide"),int(input("first number:").strip()),int(input("second number:").strip())
-    if d == "d":div(a,b)
-    if d == "a":add(a,b)
-    if d == "s":sub(a,b)
-    if d == "m":mult(a,b)
-    else:
+    d = input("(a)add,(s)subtract,(m)multiply,(d)divide,(q)quit")
+    if d == 'd':a,b = int(input("first number:").strip()),int(input("second number:").strip());div(a,b)
+    elif d == 'a':a,b = int(input("first number:").strip()),int(input("second number:").strip());add(a,b)
+    elif d == 's':a,b = int(input("first number:").strip()),int(input("second number:").strip());sub(a,b)
+    elif d == 'm':a,b = int(input("first number:").strip()),int(input("second number:").strip());mult(a,b)
+    elif d == 'q':
         if input("you done if yes type yes") == "yes":break
+    else:
+        print("bro type a,s,m,d or q")
